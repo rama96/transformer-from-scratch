1cb611c20b847f9938a5ffdb94823f1eb8f26964
Ramamurthi <ramamurthi@192.168.1.3>
2023-09-08 16:24:11 +0530

    Adding doc strings and scripts

---
+++ config.py
@@ -0,0 +1,9 @@
+model_config = {
+    'num_tokens' : 75000 ,
+    'max_len' : 512 , 
+    'embedding_dim' : 768 , 
+    'inter_dim' : 768*4 , 
+    'head_dim' : 64 , 
+    'num_heads' : 768//64 , 
+    'dropout_prob' : 0.2
+}    
